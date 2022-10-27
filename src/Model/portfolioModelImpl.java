6f53512c15d6a1908c7e7fff326b7fffe505c320
Anusha Kalbande <kalbande.a@northeastern.edu>
2022-10-27 18:20:06 -0400

    Adding new files

---
+++ src/Model/portfolioModelImpl.java
@@ -0,0 +1,80 @@
+package Model;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+public class portfolioModelImpl implements portfolioModel{
+
+  /**
+   * shares contain ticker name as key and value contains number of units, date bought
+   */
+  static HashMap<String, List<String>> shares;
+  user u = new user();
+
+  /**
+   * Contructor
+   * @param
+   */
+
+  /*public portfolioModelImpl(HashMap<String, List<String>> tickers) {
+    this.shares = tickers;
+  }*/
+
+  // get high, low, price at open time, price at close time for each ticker on today's date
+//  String[][] getPortfolioCompostion() {
+//    String[][] ans = new String[2][3];
+//
+//    for(String ticker : shares.keySet()) {
+//      AlphaVantageDemo database = new AlphaVantageDemo(ticker);
+//    }
+//    return ans;
+//  }
+
+  /**
+   * Provides the composition of a portfolio.
+   *
+   * @param portfolioName
+   * @return
+   */
+  @Override
+  public List<List<String>> getPortfolio(String portfolioName) {
+
+    return null;
+  }
+
+  /**
+   * Returns the list of portfolios of a particular user
+   *
+   * @return
+   */
+  @Override
+  public List<String> showPortfolioNames() {
+    this.u.createFolder();
+
+
+
+    String[] files = this.u.retrieveFileNames();
+    for (int i = 0; i < files.length; i++) {
+      System.out.println(files[i]);
+    }
+    return null;
+  }
+
+  /**
+   * @param portfolioName
+   * @param date
+   * @return
+   * @throws IllegalArgumentException
+   */
+  @Override
+  public double valueOfPortfolio(String portfolioName, Date date) throws IllegalArgumentException {
+    return 0;
+  }
+
+  public static void main(String args[]) {
+    portfolioModelImpl p = new portfolioModelImpl();
+    p.showPortfolioNames();
+  }
+
+}
---
+++ src/Model/user.java
@@ -0,0 +1,32 @@
+package Model;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+class user {
+  String username;
+  //String folderPath = System.getProperty("user.home") + "\\Desktop";
+  private String folderPath;
+  private File file;
+  public user() {
+    this.folderPath = "C:\\Users\\anush\\OneDrive\\Desktop\\PortFolioComposition"; //TODO change to dynamic path
+    file = new File(folderPath);
+  }
+
+  protected void createFolder() {
+    if (!Files.exists(Path.of(this.folderPath))) {
+      file.mkdir();
+      System.out.println("Folder created successfully");
+    }
+    else{
+      System.out.println("Folder already exists.");
+    }
+  }
+
+  protected String[] retrieveFileNames() {
+    return file.list();
+  }
+
+
+}
