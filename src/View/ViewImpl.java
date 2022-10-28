e13be556053f5dbb2eef18147be5bfbd12914a01
Anusha Kalbande <kalbande.a@northeastern.edu>
2022-10-28 13:10:09 -0400

    Adding scanner object

--- src/View/ViewImpl.java
+++ src/View/ViewImpl.java
@@ -1,4 +1,40 @@
 package View;
 
+import java.util.Scanner;
+
 public class ViewImpl implements ViewInterface{
+
+  Scanner scan = new Scanner(System.in);
+  @Override
+  public int displayMenu() {
+    return 0;
+  }
+
+  @Override
+  public String[] takeStockInput() {
+    System.out.println("Please enter");
+
+    return new String[0];
+  }
+
+  @Override
+  public Boolean addMoreStocks() {
+
+    return null;
+  }
+
+  @Override
+  public void displayListOfPortfolios() {
+
+  }
+
+  @Override
+  public String getPortfolioName() {
+    return null;
+  }
+
+  @Override
+  public void displayStocks() {
+
+  }
 }
