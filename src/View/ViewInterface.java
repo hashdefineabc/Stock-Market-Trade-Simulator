a8b3c19659fc6b4ba6cd6770115f7a2ee32bc68f
manasamanjunath <m.m@northeastern.edu>
2022-10-28 13:03:13 -0400

    creating view interface

--- src/View/ViewInterface.java
+++ src/View/ViewInterface.java
@@ -2,4 +2,44 @@ package View;
 
 public interface ViewInterface {
 
+  /**
+   * display options
+   * 1. create new portfolio
+   * 2. retrieve portfolio
+   * 3. check value of a particular portfolio
+   * @return the option selected by the user
+   */
+  public int displayMenu();
+
+  /**
+   * take stock details for a particular portfolio from the user
+   * It should take the ticker name and the number of units
+   * @return the ticker name and the number of units
+   */
+  public String[] takeStockInput();
+
+  /**
+   * asks user if they want to add more stocks to the portfolio
+   * @return true of they want to add more stocks else return false
+   */
+  public Boolean addMoreStocks();
+
+  /**
+   * displays the list of created portfolios
+   */
+  public void displayListOfPortfolios();
+
+  /**
+   * displays list of portfolios and asks user to select one
+   * @return the selected portfolio
+   */
+  public String getPortfolioName();
+
+  /**
+   * displays stocks of a particular portfolio
+   */
+
+  public void displayStocks();
+
+
 }
