4788640674caed4a28af3892b82c792e5c0aa759
Anusha Kalbande <kalbande.a@northeastern.edu>
2022-10-31 15:57:28 -0400

    Adding while loop for menu

--- src/Controller/ControllerImpl.java
+++ src/Controller/ControllerImpl.java
@@ -22,89 +22,90 @@ public class ControllerImpl {
 
   public void go() {
 
-    int option = view.displayMenu(); //TODO: validation for user option
+    while(true) {
+      int option = view.displayMenu(); //TODO: validation for user option
+      switch (option) {
 
-    switch (option) {
+        // create new portfolio and add stocks to the new portfolio
+        case 1:
+          view.displayMsgToUser("Creating a new portfolio...");
+          String portfolioName = view.getPortfolioNameFromUser();
+          portfolio newPortfolio = new portfolio(portfolioName);
+          while (view.addMoreStocks()) {
+            String[] s = view.takeStockInput();
+            //stock newStock = new stock(s[0], Integer.valueOf(s[1]));
+            //using builder method to create stocks
+            stock newStock = stock.getBuilder()
+                    .tickerName(s[0])
+                    .numOfUnits(Integer.valueOf(s[1]))
+                    .build();
+            newPortfolio.addStocks(newStock);
+          }
+          user.CreateNewPortfolio(newPortfolio);
+          user.savePortfolioToFile(newPortfolio);
+          if (user.checkIfFileExists(portfolioName)) {
+            view.displayMsgToUser("Portfolio saved successfully");
+          } else {
+            view.displayMsgToUser("Portfolio was not saved. Try again");
+          }
+          break;
 
-      // create new portfolio and add stocks to the new portfolio
-      case 1:
-        view.displayMsgToUser("Creating a new portfolio...");
-        String portfolioName = view.getPortfolioNameFromUser();
-        portfolio newPortfolio = new portfolio(portfolioName);
-        while(view.addMoreStocks()) {
-          String[] s = view.takeStockInput();
-          //stock newStock = new stock(s[0], Integer.valueOf(s[1]));
-          //using builder method to create stocks
-          stock newStock = stock.getBuilder()
-                                .tickerName(s[0])
-                                .numOfUnits(Integer.valueOf(s[1]))
-                                .build();
-          newPortfolio.addStocks(newStock);
-        }
-        user.CreateNewPortfolio(newPortfolio);
-        user.savePortfolioToFile(newPortfolio);
-        if (user.checkIfFileExists(portfolioName)) {
-          view.displayMsgToUser("Portfolio saved successfully");
-        }
-        else {
-          view.displayMsgToUser("Portfolio was not saved. Try again");
-        }
-        break;
+        //retrieve portfolio
+        case 2:
+          view.displayMsgToUser("Following are the portfolios created till now:");
+          List<String> portfolioNames = new ArrayList<>();
+          List<portfolio> portfolioObjects = user.getPortfoliosCreated();
+          for (portfolio p : portfolioObjects) {
+            portfolioNames.add(p.nameOfPortFolio);
+          }
+          view.displayListOfPortfolios(portfolioNames);
 
-      //retrieve portfolio
-      case 2:
-        view.displayMsgToUser("Following are the portfolios created till now:");
-        List<String> portfolioNames = new ArrayList<>();
-        List<portfolio> portfolioObjects = user.getPortfoliosCreated();
-        for (portfolio p: portfolioObjects) {
-          portfolioNames.add(p.nameOfPortFolio);
-        }
-        view.displayListOfPortfolios(portfolioNames);
-
-        int portfolioIndex = view.getPortfolioName();
-        portfolio toDisplay = user.getPortfoliosCreated().get(portfolioIndex-1);
-        List<String[]> stocksToDisplay = toDisplay.toListOfString();
-        view.displayStocks(stocksToDisplay);
-        break;
+          int portfolioIndex = view.getPortfolioName();
+          portfolio toDisplay = user.getPortfoliosCreated().get(portfolioIndex - 1);
+          List<String[]> stocksToDisplay = toDisplay.toListOfString();
+          view.displayStocks(stocksToDisplay);
+          break;
 
         // value of a particular portfolio
-      case 3:
-        List<String> stocksNamesToDisplay = new ArrayList<>();
-        for(portfolio p : user.getPortfoliosCreated()) {
-          stocksNamesToDisplay.add(p.getNameOfPortFolio());
-        }
-        view.displayListOfPortfolios(stocksNamesToDisplay);
-        int portfolioIndexForVal = view.getPortfolioName();
-        portfolio toCalcVal = user.getPortfoliosCreated().get(portfolioIndexForVal-1);
+        case 3:
+          List<String> stocksNamesToDisplay = new ArrayList<>();
+          for (portfolio p : user.getPortfoliosCreated()) {
+            stocksNamesToDisplay.add(p.getNameOfPortFolio());
+          }
+          view.displayListOfPortfolios(stocksNamesToDisplay);
+          int portfolioIndexForVal = view.getPortfolioName();
+          portfolio toCalcVal = user.getPortfoliosCreated().get(portfolioIndexForVal - 1);
 
-        LocalDate date = null;
-        try {
-          date = view.getDateFromUser();
-        } catch (ParseException e) {
-          throw new RuntimeException(e);
-        }
-        LocalDate today = LocalDate.now();
-        //validation for date
-        while (date.isAfter(today)) {
-          view.displayMsgToUser("Can't get value for date greater than today");
+          LocalDate date = null;
           try {
             date = view.getDateFromUser();
           } catch (ParseException e) {
             throw new RuntimeException(e);
           }
-        }
-        if (date.equals(today)) {
-          view.displayMsgToUser("Value will be calculated based on yesterday's closing price.");
-          date = date.minusDays(1); //modify date to yesterday
-        }
+          LocalDate today = LocalDate.now();
+          //validation for date
+          while (date.isAfter(today)) {
+            view.displayMsgToUser("Can't get value for date greater than today");
+            try {
+              date = view.getDateFromUser();
+            } catch (ParseException e) {
+              throw new RuntimeException(e);
+            }
+          }
+          if (date.equals(today)) {
+            view.displayMsgToUser("Value will be calculated based on yesterday's closing price.");
+            date = date.minusDays(1); //modify date to yesterday
+          }
 
-        double val = toCalcVal.valueOfPortfolio(date);
+          double val = toCalcVal.valueOfPortfolio(date);
 
 
-        view.displayValue(val);
-        break;
+          view.displayValue(val);
+          break;
 
-      case 4: System.exit(0);
+        case 4:
+          System.exit(0);
+      }
     }
   }
 
