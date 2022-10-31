80ce1f57d8b075801a132193caed8fbeb1897191
Anusha Kalbande <kalbande.a@northeastern.edu>
2022-10-31 18:28:48 -0400

    View Testing and added validation for user input for the menu

--- test/View/ViewImplTest.java
+++ test/View/ViewImplTest.java
@@ -3,9 +3,13 @@ package View;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
 import Controller.ControllerImpl;
 import Model.User;
@@ -17,15 +21,30 @@ public class ViewImplTest {
   @Test
   public void testDisplayMenu() {
     InputStream input = null;
-    try {
-      input = new FileInputStream("viewInputTesting.txt");
-    } catch (FileNotFoundException e) {
-      fail("Input file not found");
-    }
+    String inputForOption1 = "1";
+    input = new ByteArrayInputStream(inputForOption1.getBytes());
+    OutputStream out = new ByteArrayOutputStream();
     User u = new User();
-    ViewInterface view = new ViewImpl(input); //this input stream can be used by view as well as junit tests.
+    ViewInterface view = new ViewImpl(input, new PrintStream(out)); //this input stream can be used by view as well as junit tests.
     ControllerImpl controller = new ControllerImpl(u,view);
-    controller.go();
+    controller.showMenuOnView();
+    String expectedMenu = "\t1. Create a new Portfolio\n" + "\t2. Retrieve Portfolio\n"
+            + "\t3. Check value of a Portfolio\n" +"\t4. Exit the application.\n" + "\tPick one of the options\n";
+
+    assertEquals(expectedMenu, out.toString());
+  }
+
+  @Test
+  public void testTakeStockInput() {
+    String inputForTest = "GOOG\n5\n";
+    InputStream input = new ByteArrayInputStream(inputForTest.getBytes());
+    OutputStream out = new ByteArrayOutputStream();
+    User u = new User();
+    ViewInterface view = new ViewImpl(input, new PrintStream(out));
+    ControllerImpl controller = new ControllerImpl(u, view);
+    controller.takeStockInputFromView();
+    String expectedOutput = "Enter the ticker name:\n" + "Enter the number of units purchased:\n";
+    assertEquals(expectedOutput, out.toString());
   }
 
 }
