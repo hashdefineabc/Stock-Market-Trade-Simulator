c935957ea3a1bbe803e9f74e45b3120583bfc571
Anusha Kalbande <kalbande.a@northeastern.edu>
2022-10-31 15:49:23 -0400

    View Testing

--- test/View/ViewImplTest.java
+++ test/View/ViewImplTest.java
@@ -1,7 +1,31 @@
 package View;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
+import Controller.ControllerImpl;
+import Model.User;
+
 import static org.junit.Assert.*;
 
 public class ViewImplTest {
+
+  @Test
+  public void testDisplayMenu() {
+    InputStream input = null;
+    try {
+      input = new FileInputStream("viewInputTesting.txt");
+    } catch (FileNotFoundException e) {
+      fail("Input file not found");
+    }
+    User u = new User();
+    ViewInterface view = new ViewImpl(input); //this input stream can be used by view as well as junit tests.
+    ControllerImpl controller = new ControllerImpl(u,view);
+    controller.go();
+  }
 
 }
