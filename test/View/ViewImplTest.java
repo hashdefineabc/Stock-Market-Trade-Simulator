9a1ca480255a1c5525e4097e483b76a2c68f2c94
Anusha Kalbande <kalbande.a@northeastern.edu>
2022-11-02 11:55:56 -0400

    View testing 1

--- test/View/ViewImplTest.java
+++ test/View/ViewImplTest.java
@@ -17,16 +17,16 @@ import static org.junit.Assert.*;
 public class ViewImplTest {
 
   OutputStream out;
+  ViewInterface view;
 
   @Before
   public void setup() {
     out = new ByteArrayOutputStream();
+    view = new ViewImpl(new PrintStream(out));
   }
 
   @Test
   public void testDisplayMenu() {
-    OutputStream out = new ByteArrayOutputStream();
-    ViewInterface view = new ViewImpl(new PrintStream(out));
     view.displayMenu();
     String expectedMenu = "***********************************\n" + "\t1. Create a new Portfolio\n" + "\t2. Retrieve Portfolio\n" + "\t3. Check value of a Portfolio\n" + "\t4. Exit the application.\n" + "Pick one of the options\n";
     assertEquals(expectedMenu, out.toString());
