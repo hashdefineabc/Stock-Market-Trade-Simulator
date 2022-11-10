41f3280454d65f445902d03c7addfab44c3a3c33
Anusha Kalbande <kalbande.a@northeastern.edu>
2022-11-10 16:30:48 -0500

    Flexible portfolio

--- src/Model/FlexiblePortfolio.java
+++ src/Model/FlexiblePortfolio.java
@@ -1,6 +1,48 @@
 package Model;
 
-public interface FlexiblePortfolio {
+import java.util.List;
 
+public class FlexiblePortfolio implements IflexiblePortfolio{
 
+  private final String portfolioName;
+  private List<IstockModelNew> stocks;
+
+  public FlexiblePortfolio(String portfolioName, List<IstockModelNew> stocks) {
+    this.portfolioName = portfolioName;
+    this.stocks = stocks;
+  }
+
+  @Override
+  public String getNameOfPortFolio() {
+    return this.portfolioName;
+  }
+
+  @Override
+  public List<IstockModelNew> getStocksInPortfolio() {
+    return this.stocks;
+  }
+
+  @Override
+  public Double calculateCostBasis() {
+    double costbasis = 0.0;
+    for (IstockModelNew ns: this.stocks) {
+      costbasis += ((ns.getBuyingPrice() * ns.getNumOfUnits()) + ns.getCommission());
+    }
+    return costbasis;
+  }
+
+  @Override
+  public Double calculateValue() {
+    return null;
+  }
+
+  @Override
+  public void addStocks(IstockModelNew stockToAdd) {
+    this.stocks.add(stockToAdd);
+  }
+
+  @Override
+  public void sellStocks(IstockModelNew stockToSell) {
+    this.stocks.remove(stockToSell);
+  }
 }
