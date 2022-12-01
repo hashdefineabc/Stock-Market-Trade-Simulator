9bd6138c6f04af80738932a4c01c797e52f49aac
Anusha Kalbande <kalbande.a@northeastern.edu>
2022-11-30 20:07:18 -0500

    Investment enum comments

--- src/model/InvestmentType.java
+++ src/model/InvestmentType.java
@@ -1,6 +1,17 @@
 package model;
 
+/**
+ * The enum InvestmentType indicates whether the investment has to be made by using the weights 
+ * assigned to each portfolio or using dollar cost averaging.
+ * This InvestmentType is set everytime an investment strategy is accepted from the user.
+ */
 public enum InvestmentType {
+  /**
+   * Invest in a portfolio using the weightage assigned to each stock.
+   */
   InvestByWeights,
+  /**
+   * Invest in a portfolio using dollar cost averaging.
+   */
   DCA,
 }
